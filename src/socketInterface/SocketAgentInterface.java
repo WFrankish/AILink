713c25a91c0b7bfc3f810234cdc58c1835411e92
WFrankish <wfrankish@btinternet.com>
2015-11-23 23:06:07 +0000

    SocketAgentInterface implemented. (Soon I will be able to actually test things!)

--- src/socketInterface/SocketAgentInterface.java
+++ src/socketInterface/SocketAgentInterface.java
@@ -1,45 +1,68 @@
 package socketInterface;
 
-import templates.ActionMaster;
-import templates.Agent;
-import templates.AgentInterface;
-import templates.StateMaster;
+import templates.*;
 
 import java.io.*;
 import java.net.Socket;
 
 public class SocketAgentInterface implements AgentInterface {
-  public SocketAgentInterface(String url, int port, StateMaster stateMaster, ActionMaster actionMaster){
-    url_ = url_;
+  public SocketAgentInterface(String url, int port, Agent agent, StateMaster stateMaster, ActionMaster actionMaster){
+    url_ = url;
     signUpPort_ = port;
+    agent_ = agent;
     stateMaster_ = stateMaster;
     actionMaster_ = actionMaster;
-  }
-
-  public void registerAgent(Agent agent){
-    agent_ = agent;
+    ended = false;
   }
 
   public void run(){
-    // Register agent with the game.
     try{
+      // Register agent with the game.
       Socket registrationSocket = new Socket(url_, signUpPort_);
       BufferedWriter out = new BufferedWriter(new OutputStreamWriter(registrationSocket.getOutputStream()));
       out.write(agent_.identity()+"\n");
       out.flush();
       out.close();
       BufferedReader in = new BufferedReader(new InputStreamReader(registrationSocket.getInputStream()));
+      String portS = in.readLine();
+      String debrief = in.readLine();
+      in.close();
+      registrationSocket.close();
+      int port = Integer.parseInt(portS);
+      agent_.initialState(stateMaster_.parseString(debrief));
+      // Play the game.
+      Socket socket = new Socket(url_, port);
+      out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+      in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+      while(!socket.isClosed()){
+        String stateS = in.readLine();
+        String actionsS = in.readLine();
+        State state = stateMaster_.parseString(stateS);
+        Action[] actions = actionMaster_.parseActions(actionsS);
+        Action action = agent_.decide(actions, state);
+        out.write(action.toString());
+        out.flush();
+      }
+      in.close();
+      out.close();
     }
     catch(IOException e){
       agent_.error(e.toString());
+      end();
     }
-    // Play the game.
+  }
+
+  public void end(){
+    if(!ended){
+      ended = true;
+      agent_.end();
+    }
   }
 
   private Agent agent_;
   private String url_;
   private int signUpPort_;
-  private int port_;
   private StateMaster stateMaster_;
   private ActionMaster actionMaster_;
+  private boolean ended;
 }
--- src/socketInterface/SocketGameInterface.java
+++ src/socketInterface/SocketGameInterface.java
@@ -2,27 +2,45 @@ package socketInterface;
 
 import templates.*;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class SocketGameInterface implements GameInterface {
-  public SocketGameInterface(int port, Game game, StateMaster stateMaster, ActionMaster actionMaster){
+  public SocketGameInterface(int port, Game game, ActionMaster actionMaster){
     signUpPort_ = port;
     game_ = game;
-    stateMaster_ = stateMaster;
     actionMaster_ = actionMaster;
   }
 
   @Override
-  public String requestAgent() {
-    return null;
+  public void requestAgent() {
+    try {
+      ServerSocket serverSocket = new ServerSocket(signUpPort_);
+      Socket clientSocket = serverSocket.accept();
+      BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      String agent = in.readLine();
+      in.close();
+      int port = 3;
+      State debrief = game_.registerAgent(agent, port);
+      BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+      out.write(port+"\n");
+      out.write(debrief.toString()+"\n");
+      out.flush();
+      out.close();
+    }
+    catch(IOException e){
+      game_.error(e.toString());
+    }
   }
 
   @Override
-  public Action requestAction(String agentID, State state, Action[] actions) {
+  public Action requestAction(int agentID, State state, Action[] actions) {
     return null;
   }
 
   private int signUpPort_;
   private Game game_;
-  private StateMaster stateMaster_;
   private ActionMaster actionMaster_;
 
 }
